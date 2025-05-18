7294af413ca808c3443903b227eda709d69ec9c7
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-18 22:09:12 +0300

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,4 +1,10 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    console.error('useAuth: AuthContext is undefined. Ensure useAuth is called within AuthProvider.');
+  }
+  return context;
+};
