3a76de6c3c8498d36398757eaf6b0966c15cb2a9
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-18 16:01:00 +0300

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import styles from '../styles/globals.css';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2025 Uganda Medical Cases Platform</p>
+    </footer>
+  );
+}
