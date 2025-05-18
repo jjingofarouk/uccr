1b1daab8cc9d5c3846f97b2ed3b6c7ca7d04bc7c
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-18 16:59:02 +0300

    Update CaseForm.js

--- src/components/Case/CaseForm.js
+++ src/components/Case/CaseForm.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { addCase } from '../../firebase/firestore';
-import { uploadImage } from '../../firebase/storage';
+import { uploadImage } from '../../lib/cloudinary';
 import styles from '../../styles/globals.css';
+import { auth } from '../../firebase/config';
 
 export default function CaseForm() {
   const [title, setTitle] = useState('');
