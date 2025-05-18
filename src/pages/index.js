beaf14996206aefec625544c37b63a59d7353099
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-19 00:41:40 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,20 @@ import CaseCard from '../components/Case/CaseCard';
 import { useCases } from '../hooks/useCases';
 import Navbar from '../components/Navbar';
 
-
 export default function Home() {
   const { cases } = useCases();
 
   return (
-    <div className="container">
+    <>
       <Navbar />
-      <h1>Uganda Medical Cases</h1>
-      <div className="case-list">
-        {cases.map((caseData) => (
-          <CaseCard key={caseData.id} caseData={caseData} />
-        ))}
+      <div className="container">
+        <h1>Uganda Medical Cases</h1>
+        <div className="case-list">
+          {cases.map((caseData) => (
+            <CaseCard key={caseData.id} caseData={caseData} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
