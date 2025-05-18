926989bbff601f2c77f9cb06c1da3a1850ce72f2
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-18 15:54:16 +0300

    Create ProtectedRoute.js

---
+++ src/components/Auth/ProtectedRoute.js
@@ -0,0 +1,17 @@
+import { useRouter } from 'next/router';
+import { useAuth } from '../../hooks/useAuth';
+import { useEffect } from 'react';
+
+export default function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push('/login');
+    }
+  }, [user, loading, router]);
+
+  if (loading) return <div>Loading...</div>;
+  return user ? children : null;
+}
