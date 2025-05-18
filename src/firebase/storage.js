c0eff112a4a2462d1cb19d0ba7c0d40487b9f634
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-18 17:00:44 +0300

    Delete src/firebase/storage.js

--- src/firebase/storage.js
+++
@@ -1,9 +0,0 @@
-import { storage } from './config';
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { auth } from './config';
-
-export const uploadImage = async (file, path = `cases/${auth.currentUser.uid}/${Date.now()}`) => {
-  const storageRef = ref(storage, path);
-  await uploadBytes(storageRef, file);
-  return await getDownloadURL(storageRef);
-};
