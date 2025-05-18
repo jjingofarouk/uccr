d7750a0456a2d35b4b83c93dc71f83f8ee9dae05
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-18 18:49:36 +0300

    Update edit.js

--- src/pages/profile/edit.js
+++ src/pages/profile/edit.js
@@ -2,7 +2,6 @@ import ProfileEdit from '../../components/Profile/ProfileEdit';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import ProtectedRoute from '../../components/Auth/ProtectedRoute';
-import styles from '../../styles/globals.css';
 
 export default function ProfileEditPage() {
   return (
