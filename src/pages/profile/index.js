ec95213778227350e126b40472301299e88c48b6
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-18 16:04:19 +0300

    Create index.js

---
+++ src/pages/profile/index.js
@@ -0,0 +1,22 @@
+import ProfileCard from '../../components/Profile/ProfileCard';
+import { useAuth } from '../../hooks/useAuth';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import ProtectedRoute from '../../components/Auth/ProtectedRoute';
+import styles from '../../styles/globals.css';
+
+export default function Profile() {
+  const { user } = useAuth();
+
+  if (!user) return <div>Loading...</div>;
+
+  return (
+    <ProtectedRoute>
+      <div className="container">
+        <Navbar />
+        <ProfileCard userData={user} />
+        <Footer />
+      </div>
+    </ProtectedRoute>
+  );
+}
