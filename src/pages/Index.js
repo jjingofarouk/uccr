4deee487ab3c11e5df760fa8f0e20061e8d92249
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-18 16:01:34 +0300

    Create Index.js

---
+++ src/pages/Index.js
@@ -0,0 +1,22 @@
+import CaseCard from '../components/Case/CaseCard';
+import { useCases } from '../hooks/useCases';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import styles from '../styles/globals.css';
+
+export default function Home() {
+  const { cases } = useCases();
+
+  return (
+    <div className="container">
+      <Navbar />
+      <h1>Uganda Medical Cases</h1>
+      <div className="case-list">
+        {cases.map((caseData) => (
+          <CaseCard key={caseData.id} caseData={caseData} />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+}
