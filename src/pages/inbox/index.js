a23dafe46ede2ccc0f85bdc8b6d573bff4ba80a6
jjingofarouk <jjingofaroukk@gmail.com>
2025-05-18 18:49:02 +0300

    Update index.js

--- src/pages/inbox/index.js
+++ src/pages/inbox/index.js
@@ -2,7 +2,7 @@ import Inbox from '../../components/Chat/Inbox';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import ProtectedRoute from '../../components/Auth/ProtectedRoute';
-import styles from '../../styles/globals.css';
+
 
 export default function InboxPage() {
   return (
