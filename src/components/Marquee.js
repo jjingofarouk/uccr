1a8bcb0bebf0f8f51194db1f4361d3aef48cbef8
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-19 19:21:37 +0300

    Update Marquee.js

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useRef } from 'react';
 import styles from './Marquee.module.css';
-import { researchFindings } from '@/lib/researchFindings';
+import { researchFindings } from './researchFindings';
 
 export default function Marquee() {
   const marqueeRef = useRef(null);
