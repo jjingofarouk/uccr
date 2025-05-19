aecc3956ecc31fab2721cce106fe4567347cf443
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-19 19:03:33 +0300

    Update edit.js

--- src/pages/profile/edit.js
+++ src/pages/profile/edit.js
@@ -9,7 +9,6 @@ export default function ProfileEditPage() {
       <div className="container">
         <Navbar />
         <ProfileEdit />
-        <Footer />
       </div>
     </ProtectedRoute>
   );
