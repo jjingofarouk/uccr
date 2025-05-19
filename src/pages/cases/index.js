6f1af81d7e52c1587f0ab79ef52e0f0e2068c53e
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-20 01:34:07 +0300

    Update index.js

--- src/pages/cases/index.js
+++ src/pages/cases/index.js
@@ -1,22 +1,24 @@
-import CaseCard from '../../components/Case/CaseCard';
 import { useCases } from '../../hooks/useCases';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import ProtectedRoute from '../../components/Auth/ProtectedRoute';
+import CaseCard from '../../components/Case/CaseCard';
+import styles from './case.module.css'; // Import the CSS module
 
 export default function Cases() {
   const { cases } = useCases();
 
   return (
     <ProtectedRoute>
-      <div className="container">
+      <div className={styles.container}>
         <Navbar />
-        <h1>All Cases</h1>
-        <div className="case-list">
+        <h1 className={styles.title}>All Cases</h1>
+        <div className={styles['case-list']}>
           {cases.map((caseData) => (
             <CaseCard key={caseData.id} caseData={caseData} />
           ))}
         </div>
+        <Footer /> {/* Assuming Footer should be included here */}
       </div>
     </ProtectedRoute>
   );
