af0bdcc151bc486353ae2400ff2834ed8f00d8ce
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-19 19:19:08 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
+import Navbar from '../components/Navbar';
+import Marquee from '../components/Marquee'; // Import the Marquee
 import CaseCard from '../components/Case/CaseCard';
 import { useCases } from '../hooks/useCases';
-import Navbar from '../components/Navbar';
+import styles from './Home.module.css'; // Import CSS module
 
 export default function Home() {
   const { cases } = useCases();
@@ -8,9 +10,10 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="container">
-        <h1>Uganda Medical Cases</h1>
-        <div className="case-list">
+      <Marquee /> {/* Always visible, just under Navbar */}
+      <div className={styles.container}>
+        <h1 className={styles.title}>Uganda Medical Cases</h1>
+        <div className={styles.caseList}>
           {cases.map((caseData) => (
             <CaseCard key={caseData.id} caseData={caseData} />
           ))}
