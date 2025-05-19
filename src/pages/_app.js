27e3bf3b7dd26cb02230fbc9b92fdbe92b3c3624
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-20 02:28:29 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,12 +3,14 @@ import '../styles/globals.css';
 import { AuthProvider } from '../context/AuthContext';
 import Navbar from '../components/Navbar';
 import Marquee from '../components/Marquee';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </AuthProvider>
   );
 }
