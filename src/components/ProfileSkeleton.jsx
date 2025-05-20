2808ced7554a421633504b5dcae576dcb8435c29
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-20 19:12:56 +0300

    Update ProfileSkeleton.jsx

--- src/components/ProfileSkeleton.jsx
+++ src/components/ProfileSkeleton.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../../styles/ProfileSkeleton.module.css';
+import styles from '../styles/ProfileSkeleton.module.css';
 
 export default function ProfileSkeleton() {
   return (
