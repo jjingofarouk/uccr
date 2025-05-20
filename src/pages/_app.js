918f280054a35e2cfff3e07506053924cd07d539
Jjingo Farouk <jjingofarouq@gmail.com>
2025-05-20 03:50:16 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
         <Navbar />
         <Marquee />
         <Component {...pageProps} />
-        <Footer />
+
       </ThemeProvider>
     </AuthProvider>
   );
