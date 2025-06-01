428ebf91fe61739269299b01c75edc88260aaf72
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-02 01:17:23 +0300

    Update CaseCard.js

--- src/components/Case/CaseCard.js
+++ src/components/Case/CaseCard.js
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Calendar, Award, User } from 'lucide-react';
-import styles from '../../styles/caseCard.module.css';
 
 export default function CaseCard({ caseData }) {
   const renderSpecialtyTags = () => {
