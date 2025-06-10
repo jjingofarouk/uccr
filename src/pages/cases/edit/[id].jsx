d9a551659edd6a6708d3b434017032c338fe471d
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-10 08:34:50 +0300

    Update [id].jsx

--- src/pages/cases/edit/[id].jsx
+++ src/pages/cases/edit/[id].jsx
@@ -2,12 +2,12 @@ import { useRouter } from 'next/router';
 import EditCaseForm from '../../../components/Case/EditCaseForm';
 import ProtectedRoute from '../../../components/Auth/ProtectedRoute';
 import Loading from '../../../components/Loading';
-
+import styles from '../../stylescaseForm.module.css'; // Add this import
 
 export default function EditCasePage() {
   const router = useRouter();
   const { id } = router.query;
-
+  
   if (!id) {
     return (
       <section className={styles.loadingSection}>
