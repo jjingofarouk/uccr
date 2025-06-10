80deeb8dfb5fc2ccfc4e402234e8bf3dd397500f
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-11 01:17:33 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,39 @@
 import '../styles/globals.css';
 import { AuthProvider } from '../context/AuthContext';
 import { ThemeProvider } from '../context/ThemeContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Navbar from '../components/Navbar';
 import Marquee from '../components/Marquee';
 import Footer from '../components/Footer';
 
+// Initialize QueryClient with default options
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      cacheTime: 10 * 60 * 1000, // 10 minutes
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function MyApp({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <ThemeProvider>
-        <Navbar />
-        <div className="marquee-container">
-          <Marquee />
-        </div>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </ThemeProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ThemeProvider>
+          <Navbar />
+          <div className="marquee-container">
+            <Marquee />
+          </div>
+          <main>
+            <Component {...pageProps} />
+          </main>
+          <Footer />
+        </ThemeProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
 
