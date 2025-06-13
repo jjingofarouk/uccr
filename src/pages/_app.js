3faf6f9b92d37380165f3ff7ebe4ac69d3a580ed
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-13 19:20:07 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,14 +4,11 @@ import { ThemeProvider } from '../context/ThemeContext';
 import Navbar from '../components/Navbar';
 import Marquee from '../components/Marquee';
 import Footer from '../components/Footer';
-import { GoogleAnalytics } from '@next/third-parties/google'; // <-- Add this import
-
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-JGQE2H2LRK'; // Use env or fallback
+import { GoogleTagManager } from '@next/third-parties/google';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GoogleAnalytics gaId={GA_MEASUREMENT_ID} /> {/* Add this line for GA tracking */}
       <AuthProvider>
         <ThemeProvider>
           <Navbar />
@@ -24,6 +21,7 @@ function MyApp({ Component, pageProps }) {
           <Footer />
         </ThemeProvider>
       </AuthProvider>
+      <GoogleTagManager gtmId="G-JGQE2H2LRK" />
     </>
   );
 }
