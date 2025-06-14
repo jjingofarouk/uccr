58cad0e4f3281e81368563679c574da7039c8288
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-14 15:40:11 +0300

    Update useCases.js

--- src/hooks/useCases.js
+++ src/hooks/useCases.js
@@ -1,18 +1,31 @@
-
-
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { getCases, getCaseById } from '../firebase/firestore';
+
+// In-memory cache
+const casesCache = {};
 
 export const useCases = (uid = null) => {
   const [cases, setCases] = useState([]);
   const [loading, setLoading] = useState(true);
+  const cacheKey = useRef(uid);
 
   useEffect(() => {
     const fetchCases = async () => {
       setLoading(true);
+
+      // Check if cached data exists
+      if (casesCache[cacheKey.current]) {
+        setCases(casesCache[cacheKey.current]);
+        setLoading(false);
+        return;
+      }
+
       try {
         const fetchedCases = await getCases(uid);
         setCases(fetchedCases);
+
+        // Cache the fetched data
+        casesCache[cacheKey.current] = fetchedCases;
       } catch (error) {
         console.error('Error fetching cases:', error);
         setCases([]);
@@ -20,10 +33,9 @@ export const useCases = (uid = null) => {
         setLoading(false);
       }
     };
+
     fetchCases();
   }, [uid]);
 
   return { cases, getCaseById, loading };
 };
-
-
