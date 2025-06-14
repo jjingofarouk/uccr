e92e2f16a04bdac7420a91ee77608e2673e8abb2
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-14 16:16:40 +0300

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: 'https://yourdomain.com', // ← change this
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: ['/admin', '/dashboard'], // optional
+};
