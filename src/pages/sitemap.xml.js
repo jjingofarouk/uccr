766dddfde26c2331efb1f562c4776b99c5b9040a
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-14 19:51:56 +0300

    Update sitemap.xml.js

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -1,48 +1,84 @@
-const EXTERNAL_DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
+export const getServerSideProps = async ({ res }) => {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<!-- created with www.mysitemapgenerator.com -->
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.ugandacasereports.org/</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/cases</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/cases/new</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/profile/cases</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/profile</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/inbox</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/about</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/how-it-works</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/privacy</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/apps</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/auth</loc>
+    <lastmod>2025-06-14T19:39:30+01:00</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/profile/edit</loc>
+    <lastmod>2025-06-14T19:39:34+01:00</lastmod>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.ugandacasereports.org/cases/edit</loc>
+    <lastmod>2025-06-14T19:39:36+01:00</lastmod>
+    <priority>0.8</priority>
+  </url>
+</urlset>`;
 
-function generateSiteMap(posts) {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
-     <url>
-       <loc>https://jsonplaceholder.typicode.com</loc>
-     </url>
-     <url>
-       <loc>https://jsonplaceholder.typicode.com/guide</loc>
-     </url>
-     ${posts
-       .map(({ id }) => {
-         return `
-       <url>
-           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
-       </url>
-     `;
-       })
-       .join('')}
-   </urlset>
- `;
-}
-
-function SiteMap() {
-  // getServerSideProps will do the heavy lifting
-}
-
-export async function getServerSideProps({ res }) {
-  // We make an API call to gather the URLs for our site
-  const request = await fetch(EXTERNAL_DATA_URL);
-  const posts = await request.json();
-
-  // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts);
-
-  res.setHeader('Content-Type', 'text/xml');
-  // we send the XML to the browser
+  res.setHeader("Content-Type", "text/xml");
   res.write(sitemap);
   res.end();
 
   return {
     props: {},
   };
-}
+};
 
-export default SiteMap;
+export default function SiteMap() {
+  // getServerSideProps handles the response
+  return null;
+}
