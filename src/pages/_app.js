9c40cd20e57e921196dc986fbe467c0f713ff2b2
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-14 15:46:33 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,31 @@
 import '../styles/globals.css';
 import { AuthProvider } from '../context/AuthContext';
 import { ThemeProvider } from '../context/ThemeContext';
+import { CaseProvider } from '../context/CaseContext'; // Import CaseProvider
 import Navbar from '../components/Navbar';
 import Marquee from '../components/Marquee';
 import Footer from '../components/Footer';
 import { GoogleAnalytics } from '@next/third-parties/google';
-import { Analytics } from '@vercel/analytics/react'; // Add this import
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <AuthProvider>
         <ThemeProvider>
-          <Navbar />
-          <div className="marquee-container">
-            <Marquee />
-          </div>
-          <main>
-            <Component {...pageProps} />
-          </main>
+          <CaseProvider> {/* Wrap the entire app in CaseProvider */}
+            <Navbar />
+            <div className="marquee-container">
+              <Marquee />
+            </div>
+            <main>
+              <Component {...pageProps} />
+            </main>
+          </CaseProvider>
         </ThemeProvider>
       </AuthProvider>
       <GoogleAnalytics gaId="G-GLWW8HX76X" />
-      <Analytics /> {/* Add this component */}
+      <Analytics />
     </>
   );
 }
