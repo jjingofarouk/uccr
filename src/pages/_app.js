a94369f1ead452ceee42ef58e5b61d5620c6eb5b
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-17 22:03:31 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,10 +7,16 @@ import Marquee from '../components/Marquee';
 import Footer from '../components/Footer';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head'; // Import Head for adding meta tags
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" /> {/* Link to PWA manifest */}
+        <link rel="apple-touch-icon" href="/logo.jpg" /> {/* Apple Touch Icon */}
+        <meta name="theme-color" content="#2563eb" /> {/* Theme Color */}
+      </Head>
       <AuthProvider>
         <ThemeProvider>
           <CaseProvider> {/* Wrap the entire app in CaseProvider */}
@@ -21,6 +27,7 @@ export default function MyApp({ Component, pageProps }) {
             <main>
               <Component {...pageProps} />
             </main>
+            <Footer /> {/* Add Footer component */}
           </CaseProvider>
         </ThemeProvider>
       </AuthProvider>
