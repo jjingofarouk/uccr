25598c9c4c4c80c2c6efabb34f67ab6ee2007077
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-18 02:35:51 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,31 @@
+// _app.js
+
 import '../styles/globals.css';
 import { AuthProvider } from '../context/AuthContext';
 import { ThemeProvider } from '../context/ThemeContext';
-import { CaseProvider } from '../context/CaseContext'; // Import CaseProvider
+import { CaseProvider } from '../context/CaseContext';
+
 import Navbar from '../components/Navbar';
 import Marquee from '../components/Marquee';
+import Footer from '../components/Footer';
+
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { Analytics } from '@vercel/analytics/react';
-import Head from 'next/head'; // Import Head for adding meta tags
+
+import Head from 'next/head'; // ✅ Needed to inject the PWA-related meta tags
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/* ✅ Add only PWA-related meta links */}
       <Head>
-        <link rel="manifest" href="/manifest.json" /> {/* Link to PWA manifest */}
-        <link rel="apple-touch-icon" href="/logo.jpg" /> {/* Apple Touch Icon */}
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/logo.jpg" />
       </Head>
+
       <AuthProvider>
         <ThemeProvider>
-          <CaseProvider> {/* Wrap the entire app in CaseProvider */}
+          <CaseProvider>
             <Navbar />
             <div className="marquee-container">
               <Marquee />
@@ -25,9 +33,11 @@ export default function MyApp({ Component, pageProps }) {
             <main>
               <Component {...pageProps} />
             </main>
+            <Footer />
           </CaseProvider>
         </ThemeProvider>
       </AuthProvider>
+
       <GoogleAnalytics gaId="G-GLWW8HX76X" />
       <Analytics />
     </>
