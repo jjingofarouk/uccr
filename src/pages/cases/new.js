e0703f8c2351b13311a0885d786cab89680c946a
jjingofarouk <jjingofaroukk@gmail.com>
2025-06-26 01:59:54 +0300

    Update new.js

--- src/pages/cases/new.js
+++ src/pages/cases/new.js
@@ -1,9 +1,8 @@
 // pages/cases/new.jsx
-import CaseForm from '../../components/Case/CaseForm';
+import CaseForm from '../../components/CaseForm';
 import ProtectedRoute from '../../components/Auth/ProtectedRoute';
 import styles from '../../styles/caseForm.module.css';
-// Import Google Analytics components
-import { GoogleAnalytics } from '@next/third-parties/google';
+import { GoogleAnalytics } from '@next/third-parties';
 import { Analytics } from '@vercel/analytics/react';
 
 export default function NewCase() {
@@ -14,7 +13,6 @@ export default function NewCase() {
           <CaseForm />
         </main>
       </ProtectedRoute>
-      {/* Add Google Analytics components */}
       <GoogleAnalytics gaId="G-GLWW8HX76X" />
       <Analytics />
     </>
